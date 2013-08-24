73869c1ffac1141f819c1391daab1101484807de
evyee <jvalieva@yahoo.com>
2013-08-23 20:43:15 -0700

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Sticker::Application.routes.draw do
   resources :drinks
+  root 'drinks#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
